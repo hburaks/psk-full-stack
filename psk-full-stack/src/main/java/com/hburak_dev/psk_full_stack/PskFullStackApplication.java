67c38f675f75b7b02da4d101faec6f314a61c2c6
Hasan Burak Songur <123594944+hburaks@users.noreply.github.com>
2024-10-09 21:56:25 +0300

    Initialize the spring boot application

---
+++ psk-full-stack/src/main/java/com/hburak_dev/psk_full_stack/PskFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.hburak_dev.psk_full_stack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PskFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PskFullStackApplication.class, args);
+	}
+
+}
---
+++ psk-full-stack/src/test/java/com/hburak_dev/psk_full_stack/PskFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hburak_dev.psk_full_stack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PskFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
