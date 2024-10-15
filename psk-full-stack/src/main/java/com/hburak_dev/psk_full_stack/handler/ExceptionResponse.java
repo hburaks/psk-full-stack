ba0473c91caa25c557a656380994ac53093daf5d
Hasan Burak Songur <123594944+hburaks@users.noreply.github.com>
2024-10-15 20:16:00 +0300

    Add exception handler

---
+++ psk-full-stack/src/main/java/com/hburak_dev/psk_full_stack/handler/ExceptionResponse.java
@@ -0,0 +1,22 @@
+package com.hburak_dev.psk_full_stack.handler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY) // brings just the non-empty ones
+public class ExceptionResponse {
+
+    private Integer businessErrorCode;
+    private String businessErrorDescription;
+    private String error;
+    private Set<String> validationErrors;
+    private Map<String, String> errors;
+}
