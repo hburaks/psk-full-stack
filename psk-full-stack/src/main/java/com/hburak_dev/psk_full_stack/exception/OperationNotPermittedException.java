65455c84ed5011e2c66facfa63fd1ab66ed485dc
Hasan Burak Songur <123594944+hburaks@users.noreply.github.com>
2024-10-17 19:18:39 +0300

    Create custom run time exceptions

---
+++ psk-full-stack/src/main/java/com/hburak_dev/psk_full_stack/exception/OperationNotPermittedException.java
@@ -0,0 +1,11 @@
+package com.hburak_dev.psk_full_stack.exception;
+
+public class OperationNotPermittedException extends RuntimeException {
+
+    public OperationNotPermittedException() {
+    }
+
+    public OperationNotPermittedException(String message) {
+        super(message);
+    }
+}
