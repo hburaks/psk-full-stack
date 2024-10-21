5325e856d9c3362a8d05e9b291dd7f68aa7dc635
Hasan Burak Songur <123594944+hburaks@users.noreply.github.com>
2024-10-21 22:14:56 +0300

    Add repositories for the main entities

---
+++ psk-full-stack/src/main/java/com/hburak_dev/psk_full_stack/question/QuestionRepository.java
@@ -0,0 +1,7 @@
+package com.hburak_dev.psk_full_stack.question;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+}
---
+++ psk-full-stack/src/main/java/com/hburak_dev/psk_full_stack/session/SessionRepository.java
@@ -0,0 +1,7 @@
+package com.hburak_dev.psk_full_stack.session;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionRepository extends JpaRepository<Session, Integer> {
+
+}
