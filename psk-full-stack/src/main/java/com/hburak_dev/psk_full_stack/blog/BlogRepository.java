244caf5c05abacbc91e72173e65bfccbcd78032a
Hasan Burak Songur <123594944+hburaks@users.noreply.github.com>
2024-11-03 14:07:05 +0300

    Fix init error caused by blog repository

--- psk-full-stack/src/main/java/com/hburak_dev/psk_full_stack/blog/BlogRepository.java
+++ psk-full-stack/src/main/java/com/hburak_dev/psk_full_stack/blog/BlogRepository.java
@@ -3,8 +3,10 @@ package com.hburak_dev.psk_full_stack.blog;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BlogRepository extends JpaRepository<Blog, Integer> {
 
+    @Query("SELECT b FROM Blog b")
     Page<Blog> findAllBlogs(Pageable pageable);
 }
