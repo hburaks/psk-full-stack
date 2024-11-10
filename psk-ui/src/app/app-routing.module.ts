5f46cc276e9eb93253b551ba496b569cfdb2e9ec
Hasan Burak Songur <123594944+hburaks@users.noreply.github.com>
2024-11-10 12:07:02 +0300

    Create auth pages

--- psk-ui/src/app/app-routing.module.ts
+++ psk-ui/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {AuthComponent} from "./auth/auth.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register', component: AuthComponent},
+  {path: 'login', component: AuthComponent},
+  {path: 'activate-account', component: AuthComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- psk-ui/src/app/app.module.ts
+++ psk-ui/src/app/app.module.ts
@@ -4,15 +4,20 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {AuthComponent} from './auth/auth.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     HttpClient
