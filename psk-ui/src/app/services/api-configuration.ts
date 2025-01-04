99efaa6420bf5823adf85920a09ca47247e6702a
Hasan Burak Songur <123594944+hburaks@users.noreply.github.com>
2025-01-04 14:08:26 +0300

    add cors config

--- psk-ui/src/app/services/api-configuration.ts
+++ psk-ui/src/app/services/api-configuration.ts
@@ -11,7 +11,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = 'https://psk-backend-ac49.onrender.com/api/';
+  rootUrl: string = 'https://psk-backend-ac49.onrender.com/api';
 }
 
 /**
